5d97470de21ce8d35649690194ef951a323063fd
Jimmy Bonilla <jimmyb091202@gmail.com>
2024-02-07 13:48:43 -0800

    fixed quotation

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
             <div className='col-span-7 place-self-center text-center sm:text-left'>
                 <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
                     <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-500'>
-                        Hi, I'm Jimmy
+                        Hi, I am Jimmy
                     </span>
                     <br />
                     <TypeAnimation
